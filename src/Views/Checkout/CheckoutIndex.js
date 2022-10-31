a30f097732afd97d6f653850b86d15339b16dfdb
Natacha Vergara <ntchvergara@gmail.com>
2022-10-31 14:00:52 -0300

    merge main con Header

--- src/Views/Checkout/CheckoutIndex.js
+++ src/Views/Checkout/CheckoutIndex.js
@@ -1,5 +1,6 @@
 import React from 'react'
 import { Outlet } from 'react-router-dom'
+import Footer from '../../Components/Footer'
 import Header from '../../Components/Header'
 
 const index = () => {
@@ -7,6 +8,7 @@ const index = () => {
     <div>
         <Header/>
         <Outlet/>
+        <Footer/>
         
 
 
