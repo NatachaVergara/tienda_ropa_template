c8e4220f621117ed4bb78669d55388da8434624f
Natacha Vergara <ntchvergara@gmail.com>
2022-10-31 19:55:14 -0300

    emparche el problema con el outlet en Index.js

--- src/Views/Index.js
+++ src/Views/Index.js
@@ -1,12 +1,16 @@
 import React from "react";
-import { Outlet } from "react-router-dom";
+import { Outlet, useLocation } from "react-router-dom";
 import Footer from "../Components/Footer";
 import Header from "../Components/Header";
+import Hero from "../Components/Home/Hero";
 
 const Index = () => {
+  let location = useLocation();
+
   return (
     <>
-      <Header />     
+      <Header />
+      {location.pathname === "/" && <Hero />}
       <Outlet />
       <Footer />
     </>
