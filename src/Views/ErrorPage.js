9fb6f35446fd2a76facccf3a387757945b7e7d3c
Natacha Vergara <ntchvergara@gmail.com>
2022-11-07 19:15:12 -0300

    imagen 404

--- src/Views/ErrorPage.js
+++ src/Views/ErrorPage.js
@@ -1,10 +1,13 @@
 import React from 'react'
+import img404 from '../assets/img/error404_1.jpg'
 
 const ErrorPage = () => {
   const pageTitle = "Tienda Ropa - 404";
   document.title = pageTitle;
   return (
-    <div>ErrorPage</div>
+    <div className='error404'>
+      <img src={img404} alt='error 404'/>
+    </div>
   )
 }
 
