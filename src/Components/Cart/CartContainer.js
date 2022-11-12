f9588f692064101af934f094bdf9bb5ee15db6e5
Natacha Vergara <ntchvergara@gmail.com>
2022-11-12 16:16:52 -0300

    seteando el control de  manejo de envio

--- src/Components/Cart/CartContainer.js
+++ src/Components/Cart/CartContainer.js
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import { Link } from "react-router-dom";
 import { useStoreContext } from "../../Context/Store";
 import CartEmpty from "./CartEmpty";
@@ -12,9 +12,35 @@ const CartContainer = () => {
     addFavoritos,
     setItem,
     itemQuantity,
-    provincias
+    provincias,
   } = useStoreContext();
-  console.log(provincias)
+
+  const [constoEnvio, setCostoEnvio] = useState(0);
+  const [precioFinal, setPrecioFinal] = useState(0);
+
+  const setShippingFee = (value) => {
+    switch (value) {
+      case "Ciudad Autónoma de Buenos Aires":
+        setCostoEnvio(0);
+        break;
+      case "Buenos Aires":
+        setCostoEnvio(350);
+        break;
+      case "Tierra del Fuego, Antártida e Islas del Atlántico Sur":
+      case "Santa Cruz":
+        setCostoEnvio(500);
+        break;
+      default:
+        setCostoEnvio(450);
+        break;
+    }
+  };
+
+  useEffect(() => {
+    setPrecioFinal(constoEnvio + cartTotalPrice());
+  }, [constoEnvio, cartTotalPrice]);
+
+  console.log(provincias);
   const cartListEmpty = <CartEmpty />;
 
   return (
@@ -32,7 +58,11 @@ const CartContainer = () => {
 
           <div className="row">
             <section id="total" className="col-12 d-md-none">
-              <box-icon type='solid' name="shopping-bags" animation="tada"></box-icon>
+              <box-icon
+                type="solid"
+                name="shopping-bags"
+                animation="tada"
+              ></box-icon>
               TOTAL ${cartTotalPrice()}
             </section>
             <section id="item" className="col-12 col-md-6 col-lg-9">
@@ -44,19 +74,37 @@ const CartContainer = () => {
               />
             </section>
             <section id="total" className="col-12 col-md-6 col-lg-3">
-              <box-icon type='solid' name="shopping-bags" animation="tada"></box-icon>
+              <box-icon
+                type="solid"
+                name="shopping-bags"
+                animation="tada"
+              ></box-icon>
               <div>
                 <p> Cantidad de productos: {itemQuantity()} </p>
-                <select className="form-select " aria-label="Default select example">
-                  <option defaultValue className="w-50">Elegir provincia</option>
-                  {provincias.length > 0 && provincias.map(p => (
-                    <option value={p.nombre} key={p.id}> {p.nombre} </option>
-                  ))}
-
+                <select
+                  className="form-select my-3"
+                  aria-label="Default select example"
+                  onChange={(e) => setShippingFee(e.target.value)}
+                >
+                  <option defaultValue className="w-50">
+                    Elegir provincia
+                  </option>
+                  {provincias.length > 0 &&
+                    provincias.map((p) => (
+                      <option value={p.nombre} key={p.id}>
+                        {" "}
+                        {p.nombre}{" "}
+                      </option>
+                    ))}
                 </select>
-                <p>Costo de envío: $ 350</p>
-                <p> Orden total ${360 + cartTotalPrice()}</p>
-                <Link to='/startpayment/checkout' className="btn btn-outline-dark">Dirección y pago</Link>
+                <p>Costo de envío: ${constoEnvio} </p>
+                <p>Orden total ${precioFinal}</p>
+                <Link
+                  to="/startpayment/checkout"
+                  className="btn btn-outline-dark"
+                >
+                  Dirección y pago
+                </Link>
               </div>
             </section>
           </div>
